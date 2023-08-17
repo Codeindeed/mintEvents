3f223e2982d0da796a224b9fb99c2621d047ab68
Codeindeed <favouralex084@gmail.com>
2023-08-17 21:00:55 +0100

    removed error boundary

--- src/pages/errorElement.tsx
+++ src/pages/errorElement.tsx
@@ -1,18 +1,18 @@
-import { useRouteError } from "react-router-dom";
-import { useToast } from "@chakra-ui/react";
-function Errorboundary() {
-  const error = useRouteError();
-  const toast = useToast();
-  if (error) {
-    toast({
-      title: `${error}`,
-      description: error?.message,
-      status: "error",
-      duration: 9000,
-      isClosable: true,
-    });
-    console.log(error);
-  }
-}
+// import { useRouteError } from "react-router-dom";
+// import { useToast } from "@chakra-ui/react";
+// function Errorboundary() {
+//   const error = useRouteError();
+//   const toast = useToast();
+//   if (error) {
+//     toast({
+//       title: `${error}`,
+//       description: error?.message,
+//       status: "error",
+//       duration: 9000,
+//       isClosable: true,
+//     });
+//     console.log(error);
+//   }
+// }
 
-export default Errorboundary;
+// export default Errorboundary;
